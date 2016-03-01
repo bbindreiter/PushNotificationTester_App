482f2d0a941c5097e5e37a6acb43197b9a626dd0
Bernd Bindreiter <bernd.bindreiter@gmail.com>
2016-03-01 18:57:09 +0100

    added sample junit test

---
+++ app/src/test/java/com/firstrowria/pushnotificationtester/threads/SampleTest.java
@@ -0,0 +1,26 @@
+package com.firstrowria.pushnotificationtester.threads;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Bernd on 3/1/2016.
+ */
+public class SampleTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void testRun() throws Exception {
+        assertEquals(true, true);
+    }
+}
